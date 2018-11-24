80fef3ada1ff11cfd80d0bfb2dd3c0a6bd4f6006
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-24 22:40:54 +0900

    feat: add private routing

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { App } from '../components/App';
-import { LoadableHome, LoadableCounter, LoadableTodo } from '.';
+import { Auth } from '../components/Auth';
+import {
+  LoadableTop,
+  LoadableLogin,
+  LoadableHome,
+  LoadableCounter,
+  LoadableTodo,
+} from '.';
 
 export const Router = () => (
   <App>
     <Switch>
-      <Route exact path="/" component={LoadableHome} />
-      <Route path="/counter" component={LoadableCounter} />
-      <Route path="/todo" component={LoadableTodo} />
+      <Route exact path="/" component={LoadableTop} />
+      <Route path="/login" component={LoadableLogin} />
+      <Auth>
+        <Route path="/home" component={LoadableHome} />
+        <Route path="/counter" component={LoadableCounter} />
+        <Route path="/todo" component={LoadableTodo} />
+      </Auth>
     </Switch>
   </App>
 );
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,17 @@
 import loadable from 'loadable-components';
 
+export const LoadableTop = loadable(() =>
+  import(/* webpackChunkName: "Top" */ '../containers/Top').then(
+    ({ Top }) => Top,
+  ),
+);
+
+export const LoadableLogin = loadable(() =>
+  import(/* webpackChunkName: "Login" */ '../containers/Login').then(
+    ({ Login }) => Login,
+  ),
+);
+
 export const LoadableHome = loadable(() =>
   import(/* webpackChunkName: "Home" */ '../components/pages/Home').then(
     ({ Home }) => Home,
