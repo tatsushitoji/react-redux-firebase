d9b953c978bdb9c3ddcb4a989a23e76f21866c25
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-26 14:07:10 +0900

    refactor: contain form component

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { withFormik } from 'formik';
+import { withFormik, InjectedFormikProps } from 'formik';
+import { Link } from 'react-router-dom';
 import { PaperContainer } from '../../atoms/PaperContainer';
-import { AuthForm } from '../../organisms/AuthForm';
+// import { AuthForm } from '../../organisms/AuthForm';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { ILogin } from '../../../modules/auth';
 
 interface FormProps {
@@ -12,6 +16,60 @@ interface FormValues {
   email: string;
   password: string;
 }
+
+const AuthForm: React.SFC<
+  InjectedFormikProps<FormProps, FormValues>
+> = props => (
+  <>
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <TextField
+          label="email"
+          name="email"
+          value={props.values.email}
+          placeholder="input email"
+          // InputLabelProps={{
+          //   shrink: true,
+          // }}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <TextField
+          label="password"
+          name="password"
+          type="password"
+          value={props.values.password}
+          placeholder="input password"
+          // InputLabelProps={{
+          //   shrink: true,
+          // }}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          disabled={props.values.email.length <= 0 || props.isSubmitting}
+        >
+          login
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={props.handleReset}
+        >
+          reset
+        </Button>
+      </div>
+    </form>
+    <Typography variant="overline">
+      or <Link to="signup">Signup</Link>
+    </Typography>
+  </>
+);
 
 const LoginForm = withFormik<FormProps, FormValues>({
   mapPropsToValues: () => ({ email: '', password: '' }),
