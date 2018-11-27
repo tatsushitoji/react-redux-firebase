7d882e70d27e134b1bedb0f9682912ab3fc336ad
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-27 17:15:21 +0900

    fix: type

--- src/hocs/getDerivedStateFromProps/getDerivedStateFromProps.tsx
+++ src/hocs/getDerivedStateFromProps/getDerivedStateFromProps.tsx
@@ -8,16 +8,16 @@ type DerivedStateFromPropsFunction<TProps, TState> = (
 export const getDerivedStateFromProps = <T extends {}>(
   f: DerivedStateFromPropsFunction<T, T | {}>,
 ) => (BaseComponent: React.ComponentType<T>) => {
-  class EnhancedComponent extends React.Component {
+  class EnhancedComponent extends React.Component<T> {
     state = {};
     static getDerivedStateFromProps(nextProps: T, prevState: T | {}) {
       return f(nextProps, prevState) || null;
     }
     render() {
       return React.createFactory(BaseComponent as React.ComponentClass<T>)({
-        ...this.props,
+        ...(this.props as any),
         ...this.state,
-      } as T);
+      });
     }
   }
   return EnhancedComponent;
